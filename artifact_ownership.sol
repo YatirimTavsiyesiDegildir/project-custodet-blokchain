1feb07bcbaa2d1b5e6dec4269ab4b135f52911b8
Dogu <dogudeniz.ugur@gmail.com>
2021-07-11 07:36:55 +0300

    Artifact Ownership done

--- artifact_ownership.sol
+++ artifact_ownership.sol
@@ -1,38 +1,38 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./artifact_attack.sol";
+import "./artifact_helper.sol";
 import "./erc721.sol";
 import "./safemath.sol";
 
 /// TODO: Replace this with natspec descriptions
-contract ZombieOwnership is ZombieHelper, ERC721 {
+contract ArtifactOwnership is ArtifactHelper, ERC721 {
 
   using SafeMath for uint256;
 
-  mapping (uint => address) zombieApprovals;
+  mapping (uint => address) artifactApprovals;
 
   function balanceOf(address _owner) external view returns (uint256) {
-    return ownerZombieCount[_owner];
+    return ownerArtifactCount[_owner];
   }
 
   function ownerOf(uint256 _tokenId) external view returns (address) {
-    return zombieToOwner[_tokenId];
+    return artifactToOwner[_tokenId];
   }
 
   function _transfer(address _from, address _to, uint256 _tokenId) private {
-    ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
-    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
-    zombieToOwner[_tokenId] = _to;
+    ownerArtifactCount[_to] = ownerArtifactCount[_to].add(1);
+    ownerArtifactCount[msg.sender] = ownerArtifactCount[msg.sender].sub(1);
+    artifactToOwner[_tokenId] = _to;
     emit Transfer(_from, _to, _tokenId);
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    require (artifactToOwner[_tokenId] == msg.sender || artifactApprovals[_tokenId] == msg.sender);
     _transfer(_from, _to, _tokenId);
   }
 
   function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
-    zombieApprovals[_tokenId] = _approved;
+    artifactApprovals[_tokenId] = _approved;
     emit Approval(msg.sender, _approved, _tokenId);
   }
 
